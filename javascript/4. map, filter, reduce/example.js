d308866e0da37f7cd5246e26dfa17a526b555f3e
jinah <jinah0818@gmail.com>
2020-11-04 21:46:43 +0900

    reduce

--- javascript/4. map, filter, reduce/example.js
+++ javascript/4. map, filter, reduce/example.js
@@ -75,3 +75,27 @@ console.log(
     })()
   )
 );
+
+const nums = [1, 2, 3, 4, 5];
+
+let total = 0;
+for (const n of nums) {
+  total = total + n;
+}
+console.log(total);
+
+const reduce = (f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+};
+const add = (a, b) => a + b;
+console.log(reduce(add, 0, [1, 2, 3, 4, 5])); // 결과: 15
+console.log(reduce(add, [1, 2, 3, 4, 5])); // 결과: 15
+
+console.log(reduce((total_price, product) => total_price + product.price, 0, products));
